35c7ba727add484075591a14fdb60dc1e8bf8ce9
Kanabe Aron <aronkanabe@gmail.com>
2022-10-31 09:32:27 +0200

    Added docker-compose

--- TOTP.API/APIMain.cs
+++ TOTP.API/APIMain.cs
@@ -21,11 +21,9 @@ public class ApiMain
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
 
